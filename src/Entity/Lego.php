7ac4c5c0bb8e636b658f8839c5d3d55ad0ce8f2e
Lancelot Thore <lancelot.thore@etu.unilim.fr>
2024-01-29 16:03:05 +0100

    seance2

--- src/Entity/Lego.php
+++ src/Entity/Lego.php
@@ -1,31 +1,77 @@
-<?php
+<?php 
+
 
 namespace App\Entity;
 
 class Lego {
-    private $id;
-    private $name;
-    private $collection;
+    // L’attribute #[Route] indique ici que l'on associe la route
+    // "/" à la méthode home pour que Symfony l'exécute chaque fois
+    // que l'on accède à la racine de notre site.
+    public $id;
+    public $name;
+    public $collection;
 
-    public function __construct($id, $name, $collection)
-    {
+    public $description;
+    public $price;
+    public $pieces;
+    public $boxImage;
+    public $legoImage;
+
+    function __construct($id,$name,$coll){
         $this->id = $id;
         $this->name = $name;
-        $this->collection = $collection;
+        $this->collection = $coll;
     }
- 
-    public function getId()
-    {
+
+    function getId(): int {
         return $this->id;
     }
 
-    public function getName()
-    {
+    function getName(): string {
         return $this->name;
     }
 
-    public function getCollection()
-    {
+    function getCollection(): string {
         return $this->collection;
     }
+
+
+
+
+    function getDescription(): string {
+        return $this->description;
+    }
+    function setDescription(string $desc) {
+        $this->description = $desc;
+    }
+
+    function getPrice(): int {
+        return $this->price;
+    }
+    function setPrice(int $price) {
+        $this->price = $price;
+    }
+
+    function getPieces(): int {
+        return $this->pieces;
+    }
+    function setPieces(int $pieces) {
+        $this->pieces = $pieces;
+    }
+
+    function getBoxImage(): string {
+        return $this->boxImage;
+    }
+    function setBoxImage(string $boxImage) {
+        $this->boxImage = $boxImage;
+    }
+
+    function getLegoImage(): string {
+        return $this->legoImage;
+    }
+    function setLegoImage(string $legoImage) {
+        $this->legoImage = $legoImage;
+    }
+
+
 }
